c0c7802cf9daef187f73b6baa753719f996be44b
jseop-lim <jseoplim@gmail.com>
2022-07-06 15:40:14 +0900

    Feat: map, board apps에 모델 클래스 정의

--- board/models.py
+++ board/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.DO_NOTHING)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.DO_NOTHING)
+    content = models.TextField()
--- map/models.py
+++ map/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Road(models.Model):
+    start_pos = models.ForeignKey('Position', on_delete=models.CASCADE, related_name='start_roads')
+    end_pos = models.ForeignKey('Position', on_delete=models.CASCADE, related_name='end_roads')
+    gradient = models.FloatField()
+    width = models.FloatField()
+    is_stair = models.BooleanField()
+    flatness = models.CharField(max_length=255, choices=(('상', '상'), ('중', '중'), ('하', '하')))
