245f9f428db5f1298c4a331ea75bcd608098e5ce
jseop-lim <jseoplim@gmail.com>
2022-08-08 23:27:29 +0900

    Deploy: 배포 환경에서 URL Prefix 추가

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -6,10 +6,14 @@ import environ
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
+USE_X_FORWARDED_HOST = True
+FORCE_SCRIPT_NAME = '/api'
+
+STATIC_URL = FORCE_SCRIPT_NAME + STATIC_URL
+
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env.prod')
-
 
 SECRET_KEY = env('SECRET_KEY')
 
