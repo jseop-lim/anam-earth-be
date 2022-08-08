8622059435b236290433e66f6f38183d3ab6bee7
jseop-lim <jseoplim@gmail.com>
2022-08-09 03:31:48 +0900

    Deploy: CodeDeploy 실행 파일 수정
    처음 배포하는 경우 및 반영
    Nginx는 재시작 안함

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Import secrets
+# Import configurations
 source config.sh
-
-# Stop backend docker container
-docker compose down anam-earth-backend
 
 # Install backend docker image and erase previous image
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_ACCESS_TOKEN
 docker compose pull
+
+if [ "$(docker ps -aq -f name=anam-earth-backend)" ]; then
+    # Stop and remove backend container
+    docker compose rm backend -sf
+fi
+
+# Create and run docker containers
+docker compose up -d
 docker image prune -f
-docker images
-
-# Run backend docker container
-docker compose up anam-earth-backend -d
+# Migrate Database
 docker exec -it anam-earth-backend bash -c "python manage.py migrate --settings=config.settings.prod"
-
 # Reload Nginx in docker container
 docker exec -it nginx bash -c "nginx -s reload"
-nginx -s reload
