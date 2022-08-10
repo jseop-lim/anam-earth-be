eb3436f1c8a181ff766814f7006c7480b3539a10
jseop-lim <jseoplim@gmail.com>
2022-08-10 21:52:21 +0900

    Feat: board와 map 앱 swagger url 분리

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,27 +1,42 @@
-from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 
 from rest_framework import permissions
-
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-schema_view = get_schema_view(
+board_schema_view = get_schema_view(
     openapi.Info(
-        title='Anam Earth API',
+        title='Anam-Earth Board API',
         default_version='develop',
-        description='API endpoints for anam-earth backend server',
+        description='안암어스 게시판 API',
         contact=openapi.Contact(email="jseoplim@gmail.com"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
+    patterns=[
+        path('board/', include('board.urls'))  # url_pattern 의존
+    ]
 )
 
+map_schema_view = get_schema_view(
+    openapi.Info(
+        title='Anam-Earth Map API',
+        default_version='develop',
+        description='안암어스 지도 API',
+        contact=openapi.Contact(email="jseoplim@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    patterns=[
+        path('map/', include('map.urls'))  # url_pattern 의존
+    ]
+)
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('board/swagger', board_schema_view.with_ui('swagger', cache_timeout=0), name='board-schema-swagger-ui'),
+    path('map/', include('map.urls')),
+    path('map/swagger', map_schema_view.with_ui('swagger', cache_timeout=0), name='map-schema-swagger-ui'),
 ]
