75923a7d40835db389b45389d453246b5e364b78
jseop-lim <jseoplim@gmail.com>
2022-08-12 16:36:29 +0900

    Fix: 지도 모델 재정의
    PointField 적용
    모델 이름 및 필드 변경

--- backend/map/models.py
+++ backend/map/models.py
@@ -1,17 +1,20 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 
-class Position(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+class Node(models.Model):
+    name = models.CharField(verbose_name='노드 이름', max_length=255)
+    # address = models.CharField(max_length=255)
+    location = models.PointField(verbose_name='노드 위치(좌표)', srid=4326)
 
 
-class Road(models.Model):
-    start_pos = models.ForeignKey('Position', on_delete=models.CASCADE, related_name='start_roads')
-    end_pos = models.ForeignKey('Position', on_delete=models.CASCADE, related_name='end_roads')
-    gradient = models.FloatField()
-    width = models.FloatField()
-    is_stair = models.BooleanField()
-    flatness = models.CharField(max_length=255, choices=(('상', '상'), ('중', '중'), ('하', '하')))
+class Arc(models.Model):
+    QUALITY_CHOICES = [('상', '상'), ('중', '중'), ('하', '하')]
+    start_node = models.ForeignKey(verbose_name='시작 노드', to='Node', on_delete=models.CASCADE,
+                                   related_name='start_arcs')
+    end_node = models.ForeignKey(verbose_name='끝 노드', to='Node', on_delete=models.CASCADE,
+                                 related_name='end_arcs')
+    vertical_distance = models.FloatField(verbose_name='수직 거리')
+    horizontal_distance = models.FloatField(verbose_name='수평 거리')
+    is_stair = models.BooleanField(verbose_name='계단 여부')
+    is_step = models.BooleanField(verbose_name='단차 여부', default=False)
+    quality = models.CharField(verbose_name='정성 지표', max_length=255, choices=QUALITY_CHOICES)
