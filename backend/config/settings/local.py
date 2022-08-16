422b51483fcc0d69122c6d0a9d846fd92538cae5
jseop-lim <jseoplim@gmail.com>
2022-08-16 18:26:34 +0900

    Chore: SQL 로깅 설정 주석 처리

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -23,3 +23,20 @@ DATABASES = {
         'PORT': 3306,
     }
 }
+
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     },
+# }
