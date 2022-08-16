50a884fa23f9aaf672d0fb781ffd055019e7c396
jseop-lim <jseoplim@gmail.com>
2022-08-17 02:11:51 +0900

    Feat: 간선 목록 페이징 링크에 first, last 추가
    paging 관련 query parameter 이름을 limit와 offset으로 변경
    HateoasGeoJsonPagination에 get_first_link(), get_last_link() 정의

--- backend/map/pagination.py
+++ backend/map/pagination.py
@@ -2,13 +2,14 @@ from collections import OrderedDict
 
 from rest_framework import pagination
 from rest_framework.response import Response
+from rest_framework.utils.urls import remove_query_param, replace_query_param
 from drf_yasg import openapi
 from drf_yasg.inspectors import PaginatorInspector
 
 
 class HateoasGeoJsonPagination(pagination.LimitOffsetPagination):
-    limit_query_param = 'per_page'
-    offset_query_param = 'page'
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
 
     @staticmethod
     def get_link_data(url, rel):
@@ -18,10 +19,31 @@ class HateoasGeoJsonPagination(pagination.LimitOffsetPagination):
             ('link', url),
         ])
 
+    def get_first_link(self) -> str:
+        if self.offset <= 0:
+            return None
+
+        url = self.request.build_absolute_uri()
+        url = replace_query_param(url, self.limit_query_param, self.limit)
+
+        return remove_query_param(url, self.offset_query_param)
+
+    def get_last_link(self) -> str:
+        if self.offset + self.limit >= self.count:
+            return None
+
+        url = self.request.build_absolute_uri()
+        url = replace_query_param(url, self.limit_query_param, self.limit)
+
+        offset = self.count - (self.count - self.offset) % self.limit
+        return replace_query_param(url, self.offset_query_param, offset)
+
     def get_paginated_response(self, data):
         links = [
+            (self.get_first_link(), 'first'),
             (self.get_previous_link(), 'prev'),
-            (self.get_next_link(), 'next')
+            (self.get_next_link(), 'next'),
+            (self.get_last_link(), 'last'),
         ]
         links_header = []
         links_data = []
@@ -60,7 +82,7 @@ class HateoasGeoJsonPaginatorInspector(PaginatorInspector):
                         description='인접 페이지 링크',
                         type=openapi.TYPE_OBJECT,
                         properties=OrderedDict((
-                            ('rel', openapi.Schema(type=openapi.TYPE_STRING, enum=['prev', 'next'])),
+                            ('rel', openapi.Schema(type=openapi.TYPE_STRING, enum=['first', 'prev', 'next', 'last'])),
                             ('method', openapi.Schema(type=openapi.TYPE_STRING, enum=['GET'])),
                             ('link', openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI, x_nullable=True)),
                         ))
