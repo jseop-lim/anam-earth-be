b4df24d1fcba0b99e88eb8c99c6fe3924a85d8a7
jseop-lim <jseoplim@gmail.com>
2022-08-18 18:25:26 +0900

    Deploy: Docker Compose down 실행

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,17 +2,15 @@
 
 cd ~/anam-earth/deploy
 
+# Stop and remove containers
+docker compose down
+
 # Import configurations and Docker Hub login
 source config.sh
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_ACCESS_TOKEN
 
 # Install backend docker image and erase previous image
 docker compose pull
-
-if [ "$(docker ps -aq -f name=anam-earth-backend)" ]; then
-    # Stop and remove backend container
-    docker compose rm backend -sf
-fi
 
 # Create and run docker containers
 docker compose up -d
