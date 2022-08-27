eb840679a19fdea92ec71fba8b1db7dd1af768c9
jseop-lim <jseoplim@gmail.com>
2022-08-27 15:30:07 +0900

    Style: map/serializers.py import 최적화, 변수명 변경
    line_string -> linestring

--- backend/map/serializers.py
+++ backend/map/serializers.py
@@ -1,19 +1,20 @@
 from django.contrib.gis.geos import LineString
-
-from rest_framework import serializers
-from rest_framework_gis.serializers import GeoFeatureModelSerializer, GeometrySerializerMethodField
+from rest_framework_gis.serializers import (
+    GeoFeatureModelSerializer,
+    GeometrySerializerMethodField,
+)
 
 from map.models import Arc
 
 
 class ArcListSerializer(GeoFeatureModelSerializer):
-    line_strings = GeometrySerializerMethodField()
+    linestrings = GeometrySerializerMethodField()
 
     class Meta:
         model = Arc
         fields = ['id', 'level']  # property 필드도 포함 가능
-        geo_field = 'line_strings'  # fields 리스트에 미포함 가능
+        geo_field = 'linestrings'  # fields 리스트에 미포함 가능
         id_field = False
 
-    def get_line_strings(self, obj: Arc):
+    def get_linestrings(self, obj: Arc):
         return LineString(obj.start_node.point, obj.end_node.point)
