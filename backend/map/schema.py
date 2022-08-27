9cbf12f3c678dc20c986fe350374be0f9e5225d9
jseop-lim <jseoplim@gmail.com>
2022-08-27 17:37:34 +0900

    Feat: 노드 목록 API 제작
    속도를 위해 id, name 제거
    schema.py에 response openapi schema만 분리

---
+++ backend/map/schema.py
@@ -0,0 +1,31 @@
+from django.contrib.gis.db.models import PointField, LineStringField
+
+from rest_framework_gis.schema import GeoFeatureAutoSchema
+from drf_yasg import openapi
+
+
+node_list_schema = openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'type': openapi.Schema(type=openapi.TYPE_STRING, enum=['FeatureCollection']),
+        'features': openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'type': openapi.Schema(type=openapi.TYPE_STRING, enum=['Feature']),
+                    'geometry': openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties=GeoFeatureAutoSchema.GEO_FIELD_TO_SCHEMA[PointField]
+                    )
+                }
+            )
+        )
+    }
+)
+
+arc_optimal_schema = openapi.Schema(
+    title='geometry',
+    type=openapi.TYPE_OBJECT,
+    properties=GeoFeatureAutoSchema.GEO_FIELD_TO_SCHEMA[LineStringField]
+)
