f63f92e9bbecab893896e239bb93d9191ab59f0f
jseop-lim <jseoplim@gmail.com>
2022-08-28 11:01:19 +0900

    Fix: 게시판 작성자 unique key 해제

--- backend/board/models.py
+++ backend/board/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     # user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.DO_NOTHING)  # TODO user
-    username = models.CharField(verbose_name='작성자 이름', max_length=150, unique=True)
+    username = models.CharField(verbose_name='작성자 이름', max_length=150)
     subject = models.CharField(verbose_name='게시물 제목', max_length=255)
     content = models.TextField(verbose_name='게시물 내용')
     created_at = models.DateTimeField(verbose_name='작성 일시', auto_now_add=True)
@@ -12,7 +12,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     # user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.DO_NOTHING)  # TODO user
-    username = models.CharField(verbose_name='작성자 이름', max_length=150, unique=True)
+    username = models.CharField(verbose_name='작성자 이름', max_length=150)
     post = models.ForeignKey(verbose_name='게시물', to='Post', related_name='comments', on_delete=models.CASCADE)
     content = models.TextField(verbose_name='댓글 내용')
     created_at = models.DateTimeField(verbose_name='작성 일시', auto_now_add=True)
