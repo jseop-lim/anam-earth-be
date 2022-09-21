eb5ba207282fe09ab77e55c7fa277c7938ee8081
jseop-lim <jseoplim@gmail.com>
2022-09-21 23:36:16 +0900

    Feat: 경로 API에서 거리 포함하여 응답
    경로 Mixin 추상화
    trailing comma 추가

--- backend/map/mixins.py
+++ backend/map/mixins.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.db.models import F, Q, Case, When
 from django.contrib.gis.geos import LineString
 
@@ -19,37 +21,50 @@ class BasePathMixin:
         response_serializer.is_valid(raise_exception=True)
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
-    def perform_optimal(self, start_node, end_node, ebunch_to_add):
+    def generate_graph(self, ebunch_to_add):
         graph = nx.Graph()
         graph.add_weighted_edges_from(ebunch_to_add)
-        optimal_path = nx.shortest_path(
-            graph,
-            source=start_node,
-            target=end_node,
-            weight='weight',
-            method='dijkstra'
-        )
-        return optimal_path
+        return graph
 
     def get_arc_queryset(self):
         return NotImplemented
 
+    def _get_optimal_path(self, graph, start_node, end_node) -> List[int]:
+        return nx.shortest_path(
+            graph,
+            source=start_node,
+            target=end_node,
+            weight='weight',
+            method='dijkstra',
+        )
+
+    def _get_path_distance(self, graph, path, weight='weight'):
+        return nx.path_weight(graph, path, weight)
+
     def transform_serializer(self, serializer):
         start_node = serializer.data['start_node']
         end_node = serializer.data['end_node']
-        arcs_values = self.get_arc_queryset(
-        ).values_list(
-            'start_node', 'end_node', 'weight'
-        )
-        optimal_nodes_id = self.perform_optimal(start_node, end_node, arcs_values)
 
-        preserved = Case(*[When(pk=pk, then=order) for order, pk in enumerate(optimal_nodes_id)])
-        optimal_nodes_point = Node.objects.filter(
-            pk__in=optimal_nodes_id
+        weighted_graph = self.generate_graph(
+            self.get_arc_queryset().values_list('start_node', 'end_node', 'weight'),
+        )
+        distance_graph = self.generate_graph(
+            self.get_arc_queryset().values_list('start_node', 'end_node', 'horizontal_distance'),
+        )
+        optimal_path_nodes = self._get_optimal_path(weighted_graph, start_node, end_node)
+
+        preserved = Case(*[When(pk=pk, then=order) for order, pk in enumerate(optimal_path_nodes)])
+        optimal_path_points = Node.objects.filter(
+            pk__in=optimal_path_nodes,
         ).order_by(preserved).values_list('point', flat=True)
 
-        linestring = LineString(list(optimal_nodes_point))
-        return GeometrySerializer(data={'geometry': linestring})
+        data = {
+            'geometry': LineString(list(optimal_path_points)),
+            'properties': {
+                'distance': self._get_path_distance(distance_graph, optimal_path_nodes),
+            },
+        }
+        return GeometrySerializer(data=data)
 
 
 class OptimalPathMixin(BasePathMixin):
@@ -73,5 +88,5 @@ class ShortestPathMixin(BasePathMixin):
 
     def get_arc_queryset(self):
         return Arc.objects.annotate(
-            weight=F('horizontal_distance')
+            weight=F('horizontal_distance'),
         )
